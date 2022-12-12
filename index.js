985068ac2787c32b1dc46798e7a9dff9f85da085
Sean Coleman <scoleman2296@gmail.com>
2022-12-12 03:05:59 -0500

    styling and images

--- index.js
+++ index.js
@@ -2,29 +2,37 @@ const searchBar = document.getElementById('search-text')
 const searchBtn = document.getElementById('search-btn')
 const main = document.getElementById("main")
 
-    searchBtn.addEventListener('click', function() {
-        fetch(`http://www.omdbapi.com/?s=${searchBar.value}&apikey=b0dd4daf`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            for (let movie of data.Search) {
+searchBtn.addEventListener('click', getMovieList)
 
-                main.innerHTML += `
-               <div class="movie-container">
-                   <img src="${movie.Poster}" alt="">
-                   <div class="movie-text">
-                       <h2 class="movie-title">${movie.Title}</h2>
-                       <span class="rating">1</span>
-                       <span class="movie-length">345</span>
-                       <span class="genres">Action</span>
-                       <button class="add-to-watchlist">Watchlist</button>
-                       <p class="movie-description">The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.</p>
-                   </div>
-               </div>
-               `
-            }
+function getMovieList() {
+    main.innerHTML = ''
+    fetch(`http://www.omdbapi.com/?s=${searchBar.value}&apikey=b0dd4daf`)
+    .then(res => res.json())
+    .then(movies => {
+        const movieList = movies.Search.map(movie => movie.imdbID)
+        movieList.forEach(movie => {
+            fetch(`http://www.omdbapi.com/?i=${movie}&apikey=b0dd4daf`)
+                .then(res => res.json())
+                .then(data => {
+                    main.innerHTML += `
+                    <div class="movie-container">
+                        <img src="${data.Poster}" alt="">
+                        <div class="movie-text">
+                            <h2 class="movie-title">${data.Title}</h2>
+                            <span class="rating">${data.Ratings[0].Value}</span>
+                            <span class="movie-length">${data.Runtime}</span>
+                            <span class="genres">${data.Genre}</span>
+                            <button class="add-to-watchlist">Watchlist</button>
+                            <p class="movie-description">${data.Plot}</p>
+                        </div>
+                    </div>
+                    `
+                })
         })
     })
+}
+
+    
 
  
 
